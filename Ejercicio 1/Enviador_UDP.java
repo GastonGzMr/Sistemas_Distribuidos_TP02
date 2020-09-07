1917f86f572489e92c4de8e4a922dd0063b8e3e8
Gastón Gomariz Moser <54191173+GastonGzMr@users.noreply.github.com>
2020-09-07 16:22:22 -0300

    Add files via upload

---
+++ Ejercicio 1/Enviador_UDP.java
@@ -0,0 +1,25 @@
+import java.net.*;
+
+/**
+ *
+ * @author Gastón
+ */
+
+//El objetivo de esta clase es enviar mensajes de texto mediante sockets UDP 
+//conectados en un puerto y dirección conocidos
+public class Enviador_UDP {
+    
+    public static void main(String[] args) throws Exception {
+        //Componentes del proceso
+        DatagramSocket socket = new DatagramSocket();
+        String mensajeAEnviar = "[Mensaje]";
+        
+        //Datos del servidor
+        int puertoServidor = 1200;
+        InetAddress direccionServidor = InetAddress.getByName("localhost");
+        
+        //Envía el mensaje 
+        DatagramPacket paqueteAEnviar = new DatagramPacket(mensajeAEnviar.getBytes(), mensajeAEnviar.length(), direccionServidor, puertoServidor);
+        socket.send(paqueteAEnviar);
+    }
+}
